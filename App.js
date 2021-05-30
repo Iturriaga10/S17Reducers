341103dada10ca7f20931e565b476d94deab8178
David Iturriaga Sotelo <alfredo.reyes@sat.gob.mx>
2021-05-30 11:55:08 -0500

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const number = 0;
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button/>
+      <Text>{number}</Text>
+      <Button/>
       <StatusBar style="auto" />
     </View>
   );
